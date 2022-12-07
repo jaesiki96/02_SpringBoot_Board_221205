051c1618e824485d4f814e59609651a3234b60b0
jaesiki96 <jaesiki96@naver.com>
2022-12-07 16:07:39 +0900

    17_단일 파일 첨부 및 출력 수정 완료

--- src/main/java/com/its/board/config/WebConfig.java
+++ src/main/java/com/its/board/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private String resourcePath = "/upload/**"; // html 에서 사용할 경로
-    private String savePath = "file:///D:/springboot_img";
+    private String savePath = "file:///D:/springboot_img/";
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(resourcePath)
